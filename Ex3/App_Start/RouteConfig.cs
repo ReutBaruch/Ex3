bce7859d852e88cf4404156002905ae8a6ca171b
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2019-05-27 12:37:21 +0300

    Update RouteConfig.cs

--- Ex3/App_Start/RouteConfig.cs
+++ Ex3/App_Start/RouteConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,15 +13,19 @@ namespace Ex3
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
             /**routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "display", action = "Index2", id = UrlParameter.Optional }
             );*/
+            routes.MapRoute("display", "display/{ip}/{port}/{time}",
+            defaults: new { controller = "Flight", action = "display", ip = UrlParameter.Optional, port = UrlParameter.Optional, time = 0 });
 
             routes.MapRoute(
                 name: "Port",
-                url: "{action}/{ip}/{port}",
+                url: "{action}/{ip}/{port}/",
                 defaults: new { controller = "Flight", action = "Index", ip = UrlParameter.Optional, port = UrlParameter.Optional }
             );
 
