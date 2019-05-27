0bf9df8d9a06ed0ba86a5f24b8290bc9976b35b1
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2019-05-27 12:30:56 +0300

    Update FlightController.cs

--- Ex3/Controllers/FlightController.cs
+++ Ex3/Controllers/FlightController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,38 +15,87 @@ namespace Ex3.Controllers
         private string result1;
 
         // GET: Flight
+ 
+
         [HttpGet]
-        public ActionResult display(string ip, int port)
+        public ActionResult display(string ip, int port, int time)
         {
-            this.connect = ConnectFlight.Instance;
-            //connect.ServerConnect(ip, port);
-            //string result = connect.SendCommands("lat");
-            result1 = "/position/longitude-deg = '-60'";
-           // double re = PhraserValue(result);
-           
+
+            ConnectFlight.Instance.ip = ip;
+            ConnectFlight.Instance.port = port.ToString();
+            ConnectFlight.Instance.time = time;
+
+           // ConnectFlight.Instance.ReadData("Lat");
+
+            Session["time"] = time;
             return View();
         }
 
         [HttpPost]
-        public string CallCommand()
+        public string GetFlightData()
         {
+            var fly = ConnectFlight.Instance.Flight;
+            return ToXml(fly);
+        }
+
+        private string ToXml(Flight flight)
+        {
+            //if (Models.ConnectFlight.Instance.IsConnect)
+            //{
+            this.connect = new ConnectFlight();
             StringBuilder sb = new StringBuilder();
             XmlWriterSettings settings = new XmlWriterSettings();
             XmlWriter writer = XmlWriter.Create(sb, settings);
-
             writer.WriteStartDocument();
-            writer.WriteStartElement("CORD");
-            writer.WriteElementString("lat",connect.PhraserValue(connect.SendCommands("lat")));
-            writer.WriteElementString("lon", connect.PhraserValue(connect.SendCommands("lon")));
+            writer.WriteStartElement("Flight");
+            //writer.WriteElementString("lat", connect.PhraserValue(connect.SendCommands("lat")));
+            //writer.WriteElementString("lon", connect.PhraserValue(connect.SendCommands("lon")));
+           
+            writer.WriteElementString("Lat", "100");
+            writer.WriteElementString("Lon", "600");
+
+            //flight.ToXml(writer);
 
             writer.WriteEndElement();
             writer.WriteEndDocument();
             writer.Flush();
 
+            Console.Write(sb.ToString());
             return sb.ToString();
-            //return connect.PhraserValue(result1);
+            //}
+            return null;
         }
 
+        [HttpPost]
+        public string CallCommand()
+        {
+            if (Models.ConnectFlight.Instance.IsConnect)
+            {
+                StringBuilder sb = new StringBuilder();
+                XmlWriterSettings settings = new XmlWriterSettings();
+                XmlWriter writer = XmlWriter.Create(sb, settings);
+                writer.WriteStartDocument();
+                writer.WriteStartElement("CORD");
+                writer.WriteElementString("lat", ConnectFlight.Instance.PhraserValue(ConnectFlight.Instance.SendCommands("Lat")));
+                writer.WriteElementString("lon", ConnectFlight.Instance.PhraserValue(ConnectFlight.Instance.SendCommands("Lon")));
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Flush();
+
+                Console.Write(sb.ToString());
+                return sb.ToString();
+            }
+            return null;
+            
+            //return connect.PhraserValue(result1);
+        }
+        [HttpPost]
+        public string Search(string data)
+        {
+            ConnectFlight.Instance.ReadData(data);
+            return ToXml(ConnectFlight.Instance.Flight);
+        }
         public string Index()
         {
             return "Welcome to our Project! Please enter a URL";
