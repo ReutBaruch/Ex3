b17940ab266195875785f824852f8cf4d154d9da
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2019-06-04 12:28:49 +0300

    Update Flight.cs

--- Ex3/Models/Flight.cs
+++ Ex3/Models/Flight.cs
@@ -9,9 +9,17 @@ namespace Ex3.Models
 {
     public class Flight
     {
+        //members
         public string Lat { get; set; }
         public string Lon { get; set; }
+        public string Rudder { get; set; }
+        public string Throttle { get; set; }
+        public string fileName { get; set; }
+        public string[] linesFromFile { get; set; }
 
+        /*
+         * write the data to xml file that will be able to read from
+         */
         public void ToXml(XmlWriter writer)
         {
             writer.WriteStartElement("Flight");
@@ -20,12 +28,7 @@ namespace Ex3.Models
             writer.WriteEndElement();
         }
 
-        double Test()
-        {
-            double x = 100;
-            return x;
-        }
-    }
 
+    }
 
 }
